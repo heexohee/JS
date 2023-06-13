7cb94aa11c10d4fa0a402e3704f726e6d3799466
heexohee <rsoy2918@naver.com>
2023-06-14 02:33:01 +0900

    set 정리

--- 2-04.set.js
+++ 2-04.set.js
@@ -2,11 +2,15 @@
 // `Set`은 값만 저장하며, 키를 저장하지 않습니다. 
 // `Set`은 값이 중복되지 않는 유일한 요소로만 구성됩니다.(집합처럼)
 // `Set`을 사용하면 다음과 같은 작업을 수행할 수 있습니다.
+// 중복된 값이 있어도 하나만 카운트 되는 듯.
+// 교집합, 차집합의 개념 
+
 
 // - 값 추가 및 검색
 // - 값 삭제
 // - 모든 값 제거
 // - `Set` 크기 및 존재 여부 확인
+
 const mySet = new Set();
 mySet.add('value1');
 mySet.add('value2'); //->만약 여시서 똑같은 값(value2가 2개)을 추가할 경우 집합처럼 중복된 값은 하나로 친다!
@@ -16,4 +20,10 @@ for (const value of mySet.values()) {
   console.log(value);
 }
 console.log(mySet.size);
+
 console.log(mySet.has('value1'));
+
+
+for (const value of mySet) {
+    console.log(value);
+  }
